7491b6ee47c917eeece91224502e17d2bde2bb78
League <league@iMac.attlocal.net>
2018-10-03 13:28:48 -0700

    initial

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -o application main.cpp -I ./ -L ./ -lSDL2 -liconv -framework Cocoa -framework Metal -framework Carbon -framework IOKit -framework CoreVideo -framework CoreAudio -framework AudioToolbox -framework ForceFeedback
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "SDL2/SDL.h"
+
+int main(int argc, char** argv){
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 900, 500, 0);
+
+    SDL_Event event;
+    bool isRunning = true;
+    while(isRunning){
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_KEYDOWN :{
+                    if(event.key.keysym.sym == SDLK_ESCAPE){
+                        isRunning = false;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    return 0;
+}
