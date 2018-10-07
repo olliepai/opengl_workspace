f69f34d9a1d5dffbf1d3c461f872208743fc6c32
League <league@WTS21.attlocal.net>
2018-10-07 14:01:38 -0700

    SDL_QUIT

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@ int main(int argc, char** argv){
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 900, 500, 0);
 
-
     SDL_Event event;
     bool isRunning = true;
     while(isRunning){
         while(SDL_PollEvent(&event)){
             switch(event.type){
-                case SDL_KEYDOWN :{
+                case SDL_QUIT: {
+                    isRunning = false;
+                }
+                case SDL_KEYDOWN: {
                     if(event.key.keysym.sym == SDLK_ESCAPE){
                         isRunning = false;
                     }
